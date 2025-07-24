66323c1d9be60273373bf3f53b18e84042d8aad9
BackRunner <dev@backrunner.top>
2025-07-24 19:37:16 +0800

    chore: fix linting issues

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,8 @@
+import type { RollupOptions } from 'rollup'
+import * as fs from 'node:fs'
+import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import json from '@rollup/plugin-json'
-import * as fs from 'node:fs'
-import type { RollupOptions } from 'rollup'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
 const external = Object.keys(pkg.dependencies || {}).concat(['fs/promises'])
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import commander from 'commander';
-import { name, version } from '../package.json';
+import commander from 'commander'
+import { name, version } from '../package.json'
 
-const program = new commander.Command();
+const program = new commander.Command()
 
-program.name(name);
-program.version(version);
+program.name(name)
+program.version(version)
 
-program.parse();
+program.parse()
