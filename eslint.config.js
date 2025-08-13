94787407bf96e8da85f03c54d4083067363027b4
BackRunner <dev@backrunner.top>
2025-08-14 01:16:58 +0800

    chore: upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -6,21 +6,6 @@ export default antfu({
     tsconfigPath: 'tsconfig.json',
   },
   formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
     markdown: 'prettier',
   },
   ignores: [
